48b6214c72616c20f5c6c2adacf732a84a2a1ab9
RahulGeorge <rahulrsngs@gmail.com>
2023-07-28 23:16:23 +0530

    added

---
+++ my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Create";
+import Update from "./Update";
+import Home from "./Home";
+import Read from "./Read";
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/update/:id" element={<Update />}></Route>
+        <Route path="/read/:id" element={<Read/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
